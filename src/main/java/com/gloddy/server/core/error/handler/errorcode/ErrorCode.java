2a3ef89913aae041f4b995b13361eee68d49dd5d
jihwan2da <wlghks8982@naver.com>
2022-07-27 18:16:13 +0900

    feat: Error 다 모아놓는 ErrorCode Create

---
+++ src/main/java/com/gloddy/server/core/error/handler/errorcode/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.gloddy.server.core.error.handler.errorcode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    TOKEN_INVALID(400, "올바르지 않는 토큰입니다."),
+    TOKEN_ERROR(400, "토큰 자체에 문제가 있습니다."),
+    TOKEN_EMPTY(400, "헤더에 토큰이 존재하지 않습니다."),
+    TOKEN_BLANK(400, "토큰이 비어져 있습니다."),
+    TOKEN_EXPIRED(412, "토큰이 만료되었습니다."),
+
+    USER_NOT_FOUND(404, "존재하지 않는 유저입니다.");
+
+    private int status;
+    private String errorMessage;
+}
