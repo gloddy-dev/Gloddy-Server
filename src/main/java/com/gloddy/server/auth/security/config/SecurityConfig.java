90ac2c35d83f97c67c39af96392152eb244f1812
jihwan2da <wlghks8982@naver.com>
2022-07-28 20:58:49 +0900

    fix: SecurityConfig 빈등록 안하는 customfilter 등록 및 web.ignore 추가

--- src/main/java/com/gloddy/server/auth/security/config/SecurityConfig.java
+++ src/main/java/com/gloddy/server/auth/security/config/SecurityConfig.java
@@ -1,8 +1,14 @@
 package com.gloddy.server.auth.security.config;
 
+import com.gloddy.server.auth.jwt.JwtTokenExtractor;
+import com.gloddy.server.auth.jwt.JwtTokenValidator;
 import com.gloddy.server.auth.jwt.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,8 +18,20 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtTokenExtractor jwtTokenExtractor;
+    private final JwtTokenValidator jwtTokenValidator;
+    private final AuthenticationProvider authenticationProvider;
+    @Value("${JWT_SECRET}")
+    private String key;
+    @Value("${JWT_HEADER}")
+    private String secretHeader;
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers(HttpMethod.POST,"/sign-up*/**", "/login*/**");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -34,10 +52,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
 
                 .authorizeRequests()
-                .antMatchers("/sign-up","/login").permitAll()
                 .anyRequest().authenticated()
                 .and()
 
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenExtractor,
+                        jwtTokenValidator,
+                        authenticationProvider,
+                        secretHeader,
+                        key), UsernamePasswordAuthenticationFilter.class);
     }
 }
