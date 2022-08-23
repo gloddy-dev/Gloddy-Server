282e926dab65f7fe20a984b7071990e25d126e92
jihwan2da <wlghks8982@naver.com>
2022-08-24 01:26:46 +0900

    feat: GroupRequest Dto 추가 및 그룹 생성 시 필요한 Request Dto 생성 및 구현

---
+++ src/main/java/com/gloddy/server/group/dto/GroupRequest.java
@@ -0,0 +1,28 @@
+package com.gloddy.server.group.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+public class GroupRequest {
+
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Getter
+    @Setter
+    public static class Create {
+        private String title;
+        private String content;
+        private LocalDate meetDate;
+        private String startTime;
+        private String endTime;
+        private String place;
+        private String place_latitude;
+        private String place_longitude;
+        private int maxUser;
+    }
+
+}
