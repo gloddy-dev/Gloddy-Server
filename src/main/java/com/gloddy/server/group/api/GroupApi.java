00249021e3443bda3953a0da9ab36f3780e50557
jihwan2da <wlghks8982@naver.com>
2022-08-31 01:26:13 +0900

    chore: Merge Conflict 해결

--- src/main/java/com/gloddy/server/group/api/GroupApi.java
+++ src/main/java/com/gloddy/server/group/api/GroupApi.java
@@ -1,16 +1,15 @@
 package com.gloddy.server.group.api;
 
 import com.gloddy.server.core.response.ApiResponse;
+import com.gloddy.server.core.response.PageResponse;
 import com.gloddy.server.group.dto.GroupRequest;
 import com.gloddy.server.group.dto.GroupResponse;
+import com.gloddy.server.group.dto.response.GetGroupResponse;
 import com.gloddy.server.group.service.GroupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +23,14 @@ public class GroupApi {
                                       @RequestBody GroupRequest.Create req) {
         GroupResponse.Create response = groupService.createGroup(captainId, req);
         return ApiResponse.ok(response);
+    }
+
+    @GetMapping("/groups")
+    public PageResponse<GetGroupResponse.GetGroup> getGroups(
+            @AuthenticationPrincipal Long userId,
+            @RequestParam int size,
+            @RequestParam int page
+    ) {
+        return groupService.getGroups(userId, size, page);
     }
 }
