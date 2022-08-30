cdfa79694c421e97563b9d9d4992feeb5ec51759
jihwan2da <wlghks8982@naver.com>
2022-08-31 00:21:13 +0900

    refactor: Group API에URL 수정

--- src/main/java/com/gloddy/server/group/api/GroupApi.java
+++ src/main/java/com/gloddy/server/group/api/GroupApi.java
@@ -9,10 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class GroupApi {
 
     private final GroupService groupService;
