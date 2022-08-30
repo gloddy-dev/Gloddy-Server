808e52b74b804c6fcac565446c7a4bb3be9121a8
twoosky <lhjae3@naver.com>
2022-08-31 03:44:01 +0900

    feat: apply 생성 서비스 구현

--- src/main/java/com/gloddy/server/apply/dto/ApplyRequest.java
+++ src/main/java/com/gloddy/server/apply/dto/ApplyRequest.java
@@ -1,2 +1,18 @@
-package com.gloddy.server.apply.dto;public class ApplyRequest {
+package com.gloddy.server.apply.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.NotBlank;
+
+public class ApplyRequest {
+
+    @Getter
+    @AllArgsConstructor
+    public static class create{
+        @NotBlank(message = "introduce is empty")
+        private String introduce;
+        @NotBlank(message = "reason is empty")
+        private String reason;
+    }
 }
