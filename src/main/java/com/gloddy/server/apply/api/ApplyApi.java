2051d5864353ca3dcf85762c474253aaa6292df7
twoosky <lhjae3@naver.com>
2022-08-31 03:44:32 +0900

    feat: apply 생성 controller 구현

--- src/main/java/com/gloddy/server/apply/api/ApplyApi.java
+++ src/main/java/com/gloddy/server/apply/api/ApplyApi.java
@@ -1,2 +1,28 @@
-package com.gloddy.server.apply.api;public class ApplyApi {
+package com.gloddy.server.apply.api;
+
+import com.gloddy.server.apply.dto.ApplyRequest;
+import com.gloddy.server.apply.dto.ApplyResponse;
+import com.gloddy.server.apply.service.ApplyService;
+import com.gloddy.server.core.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
+public class ApplyApi {
+
+    private final ApplyService applyService;
+
+    @PostMapping("/groups/{groupId}/apply")
+    public ResponseEntity<ApplyResponse.create> create(
+            @AuthenticationPrincipal Long userId,
+            @PathVariable Long groupId,
+            @RequestBody ApplyRequest.create request
+    ) {
+        ApplyResponse.create response = applyService.createApply(userId, groupId, request);
+        return ApiResponse.ok(response);
+    }
 }
