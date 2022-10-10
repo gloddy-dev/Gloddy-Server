c9eec26f87b04df1bfc3744f2125934ac9e0ccdc
twoosky <lhjae3@naver.com>
2022-10-11 00:18:29 +0900

    feat: comment API 구현

---
+++ src/main/java/com/gloddy/server/comment/api/CommentController.java
@@ -0,0 +1,38 @@
+package com.gloddy.server.comment.api;
+
+import com.gloddy.server.comment.dto.CommentRequest;
+import com.gloddy.server.comment.dto.CommentResponse;
+import com.gloddy.server.comment.service.CommentService;
+import com.gloddy.server.core.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/articles")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/{articleId}/comment")
+    public ResponseEntity<CommentResponse.Create> create(
+            @PathVariable Long articleId,
+            @RequestBody CommentRequest.Create request,
+            @AuthenticationPrincipal Long userId
+    ) {
+        CommentResponse.Create response = commentService.create(userId, articleId, request);
+        return ApiResponse.created(response);
+    }
+
+    @DeleteMapping("/{articleId}/comments/{commentId}")
+    public ResponseEntity<Void> delete(
+            @PathVariable Long articleId,
+            @PathVariable Long commentId,
+            @AuthenticationPrincipal Long userId
+    ) {
+        commentService.delete(commentId, userId, articleId);
+        return ApiResponse.noContent();
+    }
+}
