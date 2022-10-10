967c09c5321c75214a15890927cd24c6cd3a7010
twoosky <lhjae3@naver.com>
2022-10-11 00:18:11 +0900

    feat: comment 서비스 로직 추가

---
+++ src/main/java/com/gloddy/server/comment/repository/CommentJpaRepository.java
@@ -0,0 +1,9 @@
+package com.gloddy.server.comment.repository;
+
+import com.gloddy.server.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentJpaRepository extends JpaRepository<Comment, Long> {
+}
---
+++ src/main/java/com/gloddy/server/comment/service/CommentService.java
@@ -0,0 +1,60 @@
+package com.gloddy.server.comment.service;
+
+import com.gloddy.server.article.entity.Article;
+import com.gloddy.server.article.handler.ArticleHandlerImpl;
+import com.gloddy.server.auth.entity.User;
+import com.gloddy.server.auth.handler.UserHandlerImpl;
+import com.gloddy.server.comment.dto.CommentRequest;
+import com.gloddy.server.comment.dto.CommentResponse;
+import com.gloddy.server.comment.entity.Comment;
+import com.gloddy.server.comment.handler.CommentHandlerImpl;
+import com.gloddy.server.comment.repository.CommentJpaRepository;
+import com.gloddy.server.core.error.handler.errorCode.ErrorCode;
+import com.gloddy.server.core.error.handler.exception.UserBusinessException;
+import com.gloddy.server.group.entity.Group;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentJpaRepository commentJpaRepository;
+    private final CommentHandlerImpl commentHandlerImpl;
+    private final UserHandlerImpl userHandlerImpl;
+    private final ArticleHandlerImpl articleHandlerImpl;
+
+    @Transactional
+    public CommentResponse.Create create(Long userId, Long articleId, CommentRequest.Create request) {
+        User user = userHandlerImpl.findById(userId);
+        Article article = articleHandlerImpl.findById(articleId);
+        Comment comment = commentJpaRepository.save(Comment.builder()
+                .user(user)
+                .article(article)
+                .content(request.getContent())
+                .build()
+        );
+        return new CommentResponse.Create(comment.getId());
+    }
+
+    // TODO: 자신이 작성하지 않은 댓글 삭제 시 exception 처리 리팩토링
+    @Transactional
+    public void delete(Long commentId, Long userId, Long articleId) {
+        User user = userHandlerImpl.findById(userId);
+        Article article = articleHandlerImpl.findById(articleId);
+
+        Group group = article.getGroup();
+        Comment comment = commentHandlerImpl.findById(commentId);
+
+        if(comment.getUser().equals(user) || group.getUser().equals(user)) {
+            commentJpaRepository.delete(comment);
+        }
+        else {
+            throw new UserBusinessException(ErrorCode.COMMENT_USER_MISMATCH);
+        }
+
+
+    }
+
+}
