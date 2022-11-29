27c70b3d20ee0ed91cf63c8c4edb871b2c4d24bf
shoplive <shoplive@shopliveui-MacBookPro.local>
2022-11-29 14:48:37 +0900

    feat: 그룹에 대한 게시글 전체 조회 비즈니스 로직 작성

--- src/main/java/com/gloddy/server/article/service/ArticleService.java
+++ src/main/java/com/gloddy/server/article/service/ArticleService.java
@@ -5,24 +5,34 @@ import com.gloddy.server.article.dto.ArticleResponse;
 import com.gloddy.server.article.entity.Article;
 import com.gloddy.server.article.repository.ArticleJpaRepository;
 import com.gloddy.server.auth.entity.User;
+import com.gloddy.server.auth.handler.UserHandler;
 import com.gloddy.server.auth.repository.UserRepository;
+import com.gloddy.server.comment.repository.CommentJpaRepository;
 import com.gloddy.server.group.entity.Group;
+import com.gloddy.server.group.handler.GroupHandler;
 import com.gloddy.server.group.repository.GroupJpaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.gloddy.server.article.dto.ArticleResponse.*;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
     private final ArticleJpaRepository articleJpaRepository;
-    private final UserRepository userRepository;
-    private final GroupJpaRepository groupJpaRepository;
+    private final CommentJpaRepository commentJpaRepository;
+    private final UserHandler userHandler;
+    private final GroupHandler groupHandler;
 
-    public ArticleResponse.Create create(Long groupId, Long userId, ArticleRequest.Create request) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("유저 없음"));
-        Group group = groupJpaRepository.findById(groupId)
-                .orElseThrow(() -> new RuntimeException("그룹 없음"));
+    public Create create(Long groupId, Long userId, ArticleRequest.Create request) {
+        User user = userHandler.findById(userId);
+        Group group = groupHandler.findById(groupId);
 
         Article article = Article.builder()
                 .content(request.getContent())
@@ -31,10 +41,10 @@ public class ArticleService {
                 .group(group)
                 .build();
         articleJpaRepository.save(article);
-        return new ArticleResponse.Create(article.getId());
+        return new Create(article.getId());
     }
 
-    public ArticleResponse.Update update(Long articleId, Long userId, ArticleRequest.Update request) {
+    public Update update(Long articleId, Long userId, ArticleRequest.Update request) {
         Article article = articleJpaRepository.findById(articleId)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 게시글입니다."));
         if(!article.getUser().getId().equals(userId)) {
@@ -42,7 +52,7 @@ public class ArticleService {
         }
         article.update(request.getContent(), request.isNotice());
         Article updateArticle = articleJpaRepository.save(article);
-        return new ArticleResponse.Update(
+        return new Update(
                 updateArticle.getContent(),
                 updateArticle.isNotice()
         );
@@ -58,5 +68,35 @@ public class ArticleService {
         articleJpaRepository.delete(article);
     }
 
+    // TODO: 추후 좋아요 여부 구현 위해 userId 받음
+    // TODO: 그룹 이미지 추가해야됨
+    public GetPreview getPreview(Long groupId, Long userId) {
+        Group group = groupHandler.findById(groupId);
+        List<Article> articles = articleJpaRepository.findAllByGroup(group);
 
+        List<Preview> previews = articles.stream()
+            .map(this::getPreview)
+            .collect(Collectors.toList());
+
+        return new GetPreview(
+//            group.getImageUrl();
+            group.getTitle(),
+            group.getContent(),
+            previews
+        );
+    }
+
+    // TODO: 게시글 작성자 이미지 추가 해야됨.
+    private Preview getPreview(Article article) {
+        User writer = article.getUser();
+        int commentCount = commentJpaRepository.countAllByArticle(article);
+        String date = article.getCreatedAt().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+        return new Preview(
+//            writer.getImageUrl(),
+            writer.getName(),
+            date,
+            article.getContent(),
+            commentCount
+        );
+    }
 }
