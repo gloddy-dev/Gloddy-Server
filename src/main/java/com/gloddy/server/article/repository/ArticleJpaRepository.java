bdd4eaad68033e41b2867232650ff0f4c4687234
shoplive <shoplive@shopliveui-MacBookPro.local>
2022-11-29 14:47:10 +0900

    feat: 그룹에 대한 게시글 조회 쿼리 메서드 작성

--- src/main/java/com/gloddy/server/article/repository/ArticleJpaRepository.java
+++ src/main/java/com/gloddy/server/article/repository/ArticleJpaRepository.java
@@ -1,9 +1,13 @@
 package com.gloddy.server.article.repository;
 
 import com.gloddy.server.article.entity.Article;
+import com.gloddy.server.group.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ArticleJpaRepository extends JpaRepository<Article, Long> {
+    List<Article> findAllByGroup(Group group);
 }
