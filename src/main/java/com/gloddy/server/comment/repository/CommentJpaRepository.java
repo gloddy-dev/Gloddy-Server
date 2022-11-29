a8ad9aa158399bf84b5b65d7e2baec46d24e582b
shoplive <shoplive@shopliveui-MacBookPro.local>
2022-11-29 14:47:51 +0900

    feat: 게시글에 대한 댓글 수 반환 쿼리 계서드 작성

--- src/main/java/com/gloddy/server/comment/repository/CommentJpaRepository.java
+++ src/main/java/com/gloddy/server/comment/repository/CommentJpaRepository.java
@@ -1,9 +1,11 @@
 package com.gloddy.server.comment.repository;
 
+import com.gloddy.server.article.entity.Article;
 import com.gloddy.server.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentJpaRepository extends JpaRepository<Comment, Long> {
+    int countAllByArticle(Article article);
 }
