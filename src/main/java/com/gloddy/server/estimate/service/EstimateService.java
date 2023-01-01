64e218141d474a1b99423798914e62f44bda3369
jihwan2da <wlghks8982@naver.com>
2023-01-01 21:44:31 +0900

    feat: 팀원 평가 API 구현

---
+++ src/main/java/com/gloddy/server/estimate/service/EstimateService.java
@@ -0,0 +1,20 @@
+package com.gloddy.server.estimate.service;
+
+import com.gloddy.server.estimate.dto.EstimateRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class EstimateService {
+
+    private final PraiseService praiseService;
+    private final MateSaveService mateSaveService;
+
+    @Transactional
+    public void estimateInGroup(Long userId, Long groupId, EstimateRequest request) {
+        praiseService.praiseInGroup(request.getPraiseDtos(), groupId);
+        mateSaveService.save(request.getMateDto(), userId);
+    }
+}
