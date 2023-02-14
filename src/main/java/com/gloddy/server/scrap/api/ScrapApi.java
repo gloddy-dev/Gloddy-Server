38fc1de4b39d0bd823a4410b98984564540332a6
twoosky <lhjae3@naver.com>
2023-02-14 18:42:18 +0900

    feat: 모임 찜 생성, 조회, 삭제 api 구현

---
+++ src/main/java/com/gloddy/server/scrap/api/ScrapApi.java
@@ -0,0 +1,48 @@
+package com.gloddy.server.scrap.api;
+
+import com.gloddy.server.core.response.ApiResponse;
+import com.gloddy.server.core.response.PageResponse;
+import com.gloddy.server.group.dto.GroupResponse.GetGroup;
+import com.gloddy.server.scrap.dto.ScrapResponse.CreateScrap;
+import com.gloddy.server.scrap.service.ScrapService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class ScrapApi {
+    private ScrapService scrapService;
+
+    @PostMapping("/groups/{groupId}/scrap")
+    public ResponseEntity<CreateScrap> create(
+            @PathVariable Long groupId,
+            @AuthenticationPrincipal Long userId
+    ) {
+        CreateScrap response = scrapService.create(groupId, userId);
+        return ApiResponse.ok(response);
+    }
+
+    @GetMapping("/scraps")
+    public ResponseEntity<PageResponse<GetGroup>> findScrapsByUserId(
+            @AuthenticationPrincipal Long userId,
+            @RequestParam(required = false, defaultValue = "0") int page,
+            @RequestParam(required = false, defaultValue = "10") int size
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        PageResponse<GetGroup> response = scrapService.findScrapByUserId(userId, pageRequest);
+        return ApiResponse.ok(response);
+    }
+
+    @DeleteMapping("/groups/{groupId}/scraps")
+    public ResponseEntity<Void> delete(
+            @PathVariable Long groupId,
+            @AuthenticationPrincipal Long userId
+    ) {
+        scrapService.delete(groupId, userId);
+        return ApiResponse.noContent();
+    }
+}
---
+++ src/main/java/com/gloddy/server/scrap/entity/Scrap.java
@@ -0,0 +1,30 @@
+package com.gloddy.server.scrap.entity;
+
+import com.gloddy.server.auth.entity.User;
+import com.gloddy.server.group.entity.Group;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+public class Scrap {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @Builder
+    public Scrap(User user, Group group) {
+        this.user = user;
+        this.group = group;
+    }
+}
