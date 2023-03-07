81656d73e35d8f60c55d8fce4dfd13f68d46fef8
twoosky <lhjae3@naver.com>
2023-03-07 19:01:36 +0900

    feat: 유저에 대한 모임 후기 조회 쿼리 메소드 작성

--- src/main/java/com/gloddy/server/estimate/repository/MateJpaRepository.java
+++ src/main/java/com/gloddy/server/estimate/repository/MateJpaRepository.java
@@ -3,5 +3,10 @@ package com.gloddy.server.estimate.repository;
 import com.gloddy.server.estimate.entity.Mate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MateJpaRepository extends JpaRepository<Mate, Long> {
+    Long countByMateId(Long mateId);
+    List<Mate> findAllByUserId(Long userId);
+    void deleteByMateIdAndUserId(Long mateId, Long userId);
 }
