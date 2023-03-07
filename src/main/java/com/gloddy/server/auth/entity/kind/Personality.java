80299378759cb658192695722c32cb6769269f86
twoosky <lhjae3@naver.com>
2023-03-07 18:58:56 +0900

    feat: Personality enum 문자열 반환 of 메소드 작성

--- src/main/java/com/gloddy/server/auth/entity/kind/Personality.java
+++ src/main/java/com/gloddy/server/auth/entity/kind/Personality.java
@@ -1,5 +1,23 @@
 package com.gloddy.server.auth.entity.kind;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Personality {
-    KIND,TEST,TEST1
+    KIND("상냥한"),
+    CUTE("귀여운")
+    ;
+
+    private String value;
+
+    private Personality(String value) {
+        this.value = value;
+    }
+
+    public static List<String> of(List<Personality> personalities) {
+       return  personalities.stream()
+                .map(Enum::toString)
+               .collect(Collectors.toList());
+    }
 }
+
