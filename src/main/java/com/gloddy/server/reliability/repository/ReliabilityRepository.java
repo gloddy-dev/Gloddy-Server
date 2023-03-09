a262f2f471bb3cf2b837bf40c928422873203fe7
twoosky <lhjae3@naver.com>
2023-03-09 20:10:54 +0900

    feat: 신뢰도 repository 생성

---
+++ src/main/java/com/gloddy/server/reliability/repository/ReliabilityRepository.java
@@ -0,0 +1,10 @@
+package com.gloddy.server.reliability.repository;
+
+import com.gloddy.server.reliability.entity.Reliability;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ReliabilityRepository extends JpaRepository<Reliability, Long> {
+    Optional<Reliability> findByUserId(Long userId);
+}
