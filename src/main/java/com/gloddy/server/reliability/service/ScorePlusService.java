0758803ac4c3da95d42e9e78827e056b957e5eb0
twoosky <lhjae3@naver.com>
2023-03-09 20:11:35 +0900

    feat: 점수 업데이트 비즈니스로직 작성

---
+++ src/main/java/com/gloddy/server/reliability/service/ScorePlusService.java
@@ -0,0 +1,30 @@
+package com.gloddy.server.reliability.service;
+
+import com.gloddy.server.reliability.handler.ReliabilityQueryHandler;
+import com.gloddy.server.reliability.entity.Reliability;
+import com.gloddy.server.reliability.entity.vo.ReliabilityLevel;
+import com.gloddy.server.reliability.entity.vo.ScorePlusType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ScorePlusService {
+    private final ReliabilityQueryHandler reliabilityQueryHandler;
+    private final ReliabilityLevelService reliabilityLevelService;
+
+    @Transactional
+    public void plus(Long userId, ScorePlusType type) {
+        Reliability reliability = reliabilityQueryHandler.findByUserId(userId);
+        Long score = type.plusScore(reliability.getScore());
+        ReliabilityLevel level = reliabilityLevelService.upgrade(score, reliability.getLevel());
+
+        reliabilityQueryHandler.save(new Reliability(
+                reliability.getId(),
+                userId,
+                score,
+                level
+        ));
+    }
+}
