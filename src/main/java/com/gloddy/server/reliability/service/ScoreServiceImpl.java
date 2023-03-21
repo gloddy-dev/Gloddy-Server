7bb0e8a214073e8f1b6a4d28abe3b19fee109a98
twoosky <lhjae3@naver.com>
2023-03-21 18:58:21 +0900

    feat: 신뢰도 점수 update 비즈니스 로직 구현

--- src/main/java/com/gloddy/server/reliability/service/ScoreServiceImpl.java
+++ src/main/java/com/gloddy/server/reliability/service/ScoreServiceImpl.java
@@ -1,8 +1,9 @@
 package com.gloddy.server.reliability.service;
 
+import com.gloddy.server.auth.entity.User;
+import com.gloddy.server.reliability.entity.vo.ScoreMinusType;
 import com.gloddy.server.reliability.handler.ReliabilityQueryHandler;
 import com.gloddy.server.reliability.entity.Reliability;
-import com.gloddy.server.reliability.entity.vo.ReliabilityLevel;
 import com.gloddy.server.reliability.entity.vo.ScorePlusType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,21 +11,29 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-public class ScoreServiceImpl {
+public class ScoreServiceImpl implements ScoreService {
     private final ReliabilityQueryHandler reliabilityQueryHandler;
-    private final ReliabilityService reliabilityLevelService;
 
     @Transactional
-    public void plus(Long userId, ScorePlusType type) {
-        Reliability reliability = reliabilityQueryHandler.findByUserId(userId);
-        Long score = type.plusScore(reliability.getScore());
-        ReliabilityLevel level = reliabilityLevelService.upgrade(score, reliability.getLevel());
+    public Long update(User user, String scoreType) {
+        Reliability reliability = reliabilityQueryHandler.findByUser(user);
+        return updateScore(reliability.getScore(), scoreType);
+    }
 
-        reliabilityQueryHandler.save(new Reliability(
-                reliability.getId(),
-                userId,
-                score,
-                level
-        ));
+    private Long updateScore(Long originScore, String scoreType) {
+        if (ScorePlusType.isPlusType(scoreType)) {
+            return plusScore(originScore, scoreType);
+        }
+        return minusScore(originScore, scoreType);
+    }
+
+    private Long plusScore(Long originScore, String scoreType) {
+        ScorePlusType type = ScorePlusType.valueOf(scoreType);
+        return type.plusScore(originScore);
+    }
+
+    private Long minusScore(Long originScore, String scoreType) {
+        ScoreMinusType type = ScoreMinusType.valueOf(scoreType);
+        return type.minusScore(originScore);
     }
 }
