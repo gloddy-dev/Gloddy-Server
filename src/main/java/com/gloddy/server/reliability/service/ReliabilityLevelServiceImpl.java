cdd372aa8df4bb359a22a70a2f978cbec4bd29b2
twoosky <lhjae3@naver.com>
2023-03-21 18:57:18 +0900

    feat: 신뢰도 레벨 update 비즈니스 로직 구현

--- src/main/java/com/gloddy/server/reliability/service/ReliabilityLevelServiceImpl.java
+++ src/main/java/com/gloddy/server/reliability/service/ReliabilityLevelServiceImpl.java
@@ -1,13 +1,28 @@
 package com.gloddy.server.reliability.service;
 
+import com.gloddy.server.auth.entity.User;
+import com.gloddy.server.reliability.entity.Reliability;
 import com.gloddy.server.reliability.entity.vo.ReliabilityLevel;
+import com.gloddy.server.reliability.handler.ReliabilityQueryHandler;
+import com.gloddy.server.reliability.repository.ReliabilityRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ReliabilityServiceImpl implements ReliabilityService {
+@RequiredArgsConstructor
+public class ReliabilityLevelServiceImpl implements ReliabilityLevelService {
+    private final ReliabilityQueryHandler reliabilityQueryHandler;
 
     @Override
-    public ReliabilityLevel updateLevel(Long score, ReliabilityLevel level) {
-        return level.upgradeLevel(score);
+    public void update(User user, Long score) {
+        Reliability reliability = reliabilityQueryHandler.findByUser(user);
+        ReliabilityLevel level = reliability.getLevel().upgradeLevel(score);
+
+        reliabilityQueryHandler.save(new Reliability(
+                reliability.getId(),
+                reliability.getUser(),
+                score,
+                level
+        ));
     }
 }
