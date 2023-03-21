6388d86dc22f72cff0a85a7b7f531c502622520f
twoosky <lhjae3@naver.com>
2023-03-21 19:17:33 +0900

    feat: 그룹 생성 시 점수 업데이트 이벤트 발행

--- src/main/java/com/gloddy/server/group/service/GroupService.java
+++ src/main/java/com/gloddy/server/group/service/GroupService.java
@@ -5,6 +5,9 @@ import com.gloddy.server.apply.entity.vo.Status;
 import com.gloddy.server.apply.repository.ApplyJpaRepository;
 import com.gloddy.server.auth.entity.User;
 import com.gloddy.server.core.utils.event.GroupParticipateEvent;
+import com.gloddy.server.core.event.score.ScoreEventPublisher;
+import com.gloddy.server.core.event.score.ScoreUpdateEvent;
+import com.gloddy.server.reliability.entity.vo.ScoreType;
 import com.gloddy.server.user.repository.UserRepository;
 import com.gloddy.server.core.error.handler.errorCode.ErrorCode;
 import com.gloddy.server.core.error.handler.exception.UserBusinessException;
@@ -37,6 +40,7 @@ public class GroupService {
     private final ApplyJpaRepository applyJpaRepository;
     private final UserRepository userRepository;
     private final ApplicationEventPublisher eventPublisher;
+    private final ScoreEventPublisher scoreEventPublisher;
 
     // 같은 학교의 소모임만 조회 -> 소모임 개최자와 해당 사용자의 학교가 같은 경우의 소모임만 조회
     // 참가 멤버 수 -> apply 엔티티에 상태값 추가해 가져오기
@@ -97,6 +101,7 @@ public class GroupService {
         Group saveGroup = groupJpaRepository.save(buildGroup);
 
         eventPublisher.publishEvent(new GroupParticipateEvent(captainId, saveGroup.getId()));
+        scoreEventPublisher.publish(new ScoreUpdateEvent(captain, ScoreType.Created_Group));
 
         return new GroupResponse.Create(saveGroup.getId());
     }
