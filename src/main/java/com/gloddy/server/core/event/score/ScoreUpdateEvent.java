fc78cc080a6a4a068ee8432572a36d37144ece71
twoosky <lhjae3@naver.com>
2023-03-21 19:17:14 +0900

    fix: ScoreType String에서 Enum으로 변경

--- src/main/java/com/gloddy/server/core/event/score/ScoreUpdateEvent.java
+++ src/main/java/com/gloddy/server/core/event/score/ScoreUpdateEvent.java
@@ -1,6 +1,7 @@
 package com.gloddy.server.core.event.score;
 
 import com.gloddy.server.auth.entity.User;
+import com.gloddy.server.reliability.entity.vo.ScoreType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,5 +11,5 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ScoreUpdateEvent {
     private User user;
-    private String ScoreType;
+    private ScoreType type;
 }
--- src/main/java/com/gloddy/server/reliability/service/ScoreService.java
+++ src/main/java/com/gloddy/server/reliability/service/ScoreService.java
@@ -1,7 +1,40 @@
 package com.gloddy.server.reliability.service;
 
 import com.gloddy.server.auth.entity.User;
+import com.gloddy.server.reliability.entity.vo.ScoreMinusType;
+import com.gloddy.server.reliability.entity.vo.ScoreType;
+import com.gloddy.server.reliability.handler.ReliabilityQueryHandler;
+import com.gloddy.server.reliability.entity.Reliability;
+import com.gloddy.server.reliability.entity.vo.ScorePlusType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface ScoreService {
-    Long update(User user, String scoreType);
+@Service
+@RequiredArgsConstructor
+public class ScoreService {
+    private final ReliabilityQueryHandler reliabilityQueryHandler;
+
+    @Transactional
+    public Long update(User user, ScoreType type) {
+        Reliability reliability = reliabilityQueryHandler.findByUser(user);
+        return updateScore(reliability.getScore(), type);
+    }
+
+    private Long updateScore(Long originScore, ScoreType type) {
+        if (ScorePlusType.isPlusType(type.name())) {
+            return plusScore(originScore, type);
+        }
+        return minusScore(originScore, type);
+    }
+
+    private Long plusScore(Long originScore, ScoreType scoreType) {
+        ScorePlusType type = ScorePlusType.valueOf(scoreType.name());
+        return type.plusScore(originScore);
+    }
+
+    private Long minusScore(Long originScore, ScoreType scoreType) {
+        ScoreMinusType type = ScoreMinusType.valueOf(scoreType.name());
+        return type.minusScore(originScore);
+    }
 }
