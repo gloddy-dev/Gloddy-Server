a71615edc305cdb11f28fcf8da0d3e288784e3db
jihwan2da <wlghks8982@naver.com>
2023-04-04 20:28:42 +0900

    feat: 칭찬 서비스 트랜잭션에 UserGroup 칭찬 완료 로직 추가

--- src/main/java/com/gloddy/server/estimate/service/EstimateService.java
+++ src/main/java/com/gloddy/server/estimate/service/EstimateService.java
@@ -3,6 +3,7 @@ package com.gloddy.server.estimate.service;
 import com.gloddy.server.estimate.dto.EstimateRequest;
 import com.gloddy.server.estimate.service.mate.MateSaveService;
 import com.gloddy.server.estimate.service.praise.PraiseService;
+import com.gloddy.server.group.service.UserGroupUpdateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,10 +14,12 @@ public class EstimateService {
 
     private final PraiseService praiseService;
     private final MateSaveService mateSaveService;
+    private final UserGroupUpdateService userGroupUpdateService;
 
     @Transactional
     public void estimateInGroup(Long userId, Long groupId, EstimateRequest request) {
         praiseService.praiseInGroup(request.getPraiseDtos(), groupId);
         mateSaveService.save(request.getMateDto(), userId);
+        userGroupUpdateService.completePraise(userId, groupId);
     }
 }
