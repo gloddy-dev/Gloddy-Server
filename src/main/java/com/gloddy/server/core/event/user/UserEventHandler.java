12f7e3c577fa0f9e62a1e862ff92a556942a8420
twoosky <lhjae3@naver.com>
2023-04-05 15:07:19 +0900

    fix: 신뢰도 service import

--- src/main/java/com/gloddy/server/core/event/user/UserEventHandler.java
+++ src/main/java/com/gloddy/server/core/event/user/UserEventHandler.java
@@ -1,6 +1,7 @@
 package com.gloddy.server.core.event.user;
 
 import com.gloddy.server.estimate.service.praise.PraiseSaveService;
+import com.gloddy.server.reliability.service.ReliabilityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
