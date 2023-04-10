f5ee76026663ab5cb83e610c14ce06fd8d6f2b89
twoosky <lhjae3@naver.com>
2023-04-10 18:24:07 +0900

    feat: 최고의 짝꿍 신뢰도 업데이트 이벤트 발행

--- src/main/java/com/gloddy/server/estimate/service/mate/MateSaveService.java
+++ src/main/java/com/gloddy/server/estimate/service/mate/MateSaveService.java
@@ -1,6 +1,9 @@
 package com.gloddy.server.estimate.service.mate;
 
 import com.gloddy.server.auth.entity.User;
+import com.gloddy.server.core.event.score.ScoreEventPublisher;
+import com.gloddy.server.core.event.score.ScoreUpdateEvent;
+import com.gloddy.server.reliability.entity.vo.ScoreType;
 import com.gloddy.server.user.service.UserFindService;
 import com.gloddy.server.estimate.dto.MateDto;
 import com.gloddy.server.estimate.entity.Mate;
@@ -14,10 +17,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class MateSaveService {
     private final MateJpaRepository mateJpaRepository;
     private final UserFindService userFindService;
+    private final ScoreEventPublisher scoreEventPublisher;
+
 
     @Transactional
     public Mate save(MateDto mateDto, Long mateId) {
         User user = userFindService.findById(mateDto.getUserId());
+        scoreEventPublisher.publish(new ScoreUpdateEvent(user, ScoreType.Mated));
         return mateJpaRepository.save(
                 Mate.builder()
                    .mateId(mateId)
