5e8b0f39bb2ee2e46fd1a4e152cef8f496e4fb47
twoosky <lhjae3@naver.com>
2023-04-13 16:51:23 +0900

    test: ReliabilityLevel, Score 테스트 작성

---
+++ src/test/java/com/gloddy/server/reliability/UpdateReliabilityByCreateGroupTest.java
@@ -0,0 +1,10 @@
+package com.gloddy.server.reliability;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UpdateReliabilityByCreateGroupTest {
+
+}
