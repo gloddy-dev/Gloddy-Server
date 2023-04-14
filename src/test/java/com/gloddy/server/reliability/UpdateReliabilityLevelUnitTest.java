617f572d9376a6e8e24806c8bec09deb071395ac
twoosky <lhjae3@naver.com>
2023-04-14 22:22:52 +0900

    test: 신뢰도 등급 업데이트 단위 테스트

--- src/test/java/com/gloddy/server/reliability/UpdateReliabilityLevelUnitTest.java
+++ src/test/java/com/gloddy/server/reliability/UpdateReliabilityLevelUnitTest.java
@@ -1,20 +1,12 @@
 package com.gloddy.server.reliability;
 
-
-import com.gloddy.server.reliability.entity.Reliability;
 import com.gloddy.server.reliability.entity.vo.ReliabilityLevel;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
 public class UpdateReliabilityLevelUnitTest {
-
-    @Mock
-    private Reliability reliability;
 
     @Test
     public void successUpdateReliabilityLevelTest() {
