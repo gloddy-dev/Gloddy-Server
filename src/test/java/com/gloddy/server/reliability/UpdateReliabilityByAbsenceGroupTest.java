409b75832b079aa34273d5db53ffd82aba2e3596
twoosky <lhjae3@naver.com>
2023-04-18 01:33:43 +0900

    test: 그룹 불참 신뢰도 업데이트 통합 테스트 작성

---
+++ src/test/java/com/gloddy/server/reliability/UpdateReliabilityByAbsenceGroupTest.java
@@ -0,0 +1,9 @@
+package com.gloddy.server.reliability;
+
+import com.gloddy.server.common.reliability.ReliabilityApiTest;
+import org.springframework.test.context.event.RecordApplicationEvents;
+
+@RecordApplicationEvents
+public class UpdateReliabilityByAbsenceGroupTest extends ReliabilityApiTest {
+
+}
