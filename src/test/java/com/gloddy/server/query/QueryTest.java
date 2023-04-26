beebdcd5df2f30ce8084752e2b55431e7198b3fc
jihwan2da <wlghks8982@naver.com>
2023-04-27 02:29:05 +0900

    test: 쿼리 테스트를 위한 Base 클래스 설계'

---
+++ src/test/java/com/gloddy/server/query/QueryTest.java
@@ -0,0 +1,31 @@
+package com.gloddy.server.query;
+
+import com.gloddy.server.estimate.repository.PraiseJpaRepository;
+import com.gloddy.server.group.repository.GroupJpaRepository;
+import com.gloddy.server.group.repository.UserGroupJpaRepository;
+import com.gloddy.server.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+
+import javax.persistence.EntityManager;
+
+@DataJpaTest
+@Import(TestConfig.class)
+public abstract class QueryTest {
+
+    @Autowired
+    protected GroupJpaRepository groupJpaRepository;
+
+    @Autowired
+    protected UserGroupJpaRepository userGroupJpaRepository;
+
+    @Autowired
+    protected UserRepository userRepository;
+
+    @Autowired
+    protected PraiseJpaRepository praiseJpaRepository;
+
+    @Autowired
+    protected EntityManager em;
+}
