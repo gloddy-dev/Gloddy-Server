d358f2447d2a797d77e3424710183af27516ace6
jihwan2da <wlghks8982@naver.com>
2023-05-18 21:49:55 +0900

    feat: ArticleCommandHandler 생성 및 구현

---
+++ src/main/java/com/gloddy/server/article/domain/handler/ArticleCommandHandler.java
@@ -0,0 +1,8 @@
+package com.gloddy.server.article.domain.handler;
+
+import com.gloddy.server.article.domain.Article;
+
+public interface ArticleCommandHandler {
+
+    Article save(Article article);
+}
---
+++ src/main/java/com/gloddy/server/article/domain/handler/impl/ArticleCommandHandlerImpl.java
@@ -0,0 +1,19 @@
+package com.gloddy.server.article.domain.handler.impl;
+
+import com.gloddy.server.article.domain.Article;
+import com.gloddy.server.article.domain.handler.ArticleCommandHandler;
+import com.gloddy.server.article.infra.repository.ArticleJpaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ArticleCommandHandlerImpl implements ArticleCommandHandler {
+
+    private final ArticleJpaRepository articleJpaRepository;
+
+    @Override
+    public Article save(Article article) {
+        return articleJpaRepository.save(article);
+    }
+}
