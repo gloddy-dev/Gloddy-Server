79445af709589699c0aefca0f0b853aa8b8be2f2
jihwan2da <wlghks8982@naver.com>
2023-05-19 03:46:22 +0900

    feat: 게시글 삭제 정책 도메인 서비스 구현

---
+++ src/main/java/com/gloddy/server/article/domain/service/ArticleDeletePolicy.java
@@ -0,0 +1,20 @@
+package com.gloddy.server.article.domain.service;
+
+import com.gloddy.server.article.domain.Article;
+import com.gloddy.server.article.exception.NoAuthorizedArticleDeleteException;
+import com.gloddy.server.group.domain.UserGroup;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ArticleDeletePolicy {
+
+    public void validate(Article article, UserGroup userGroup) {
+        validatePermission(article, userGroup);
+    }
+
+    private void validatePermission(Article article, UserGroup userGroup) {
+        if (!article.isWriter(userGroup.getUser()) && !userGroup.isCaptain()) {
+            throw new NoAuthorizedArticleDeleteException();
+        }
+    }
+}
