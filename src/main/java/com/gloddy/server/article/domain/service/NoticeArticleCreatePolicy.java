2cbc95f1c5b7ffa1e8063b4bfb93283748289e03
jihwan2da <wlghks8982@naver.com>
2023-05-18 21:52:23 +0900

    feat: 공지 게시글 생성 정책 도메인 서비스 구현

---
+++ src/main/java/com/gloddy/server/article/domain/service/NoticeArticleCreatePolicy.java
@@ -0,0 +1,15 @@
+package com.gloddy.server.article.domain.service;
+
+import com.gloddy.server.article.exception.NoAuthorizedNoticeArticleException;
+import com.gloddy.server.group.domain.UserGroup;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NoticeArticleCreatePolicy {
+
+    public void validate(UserGroup userGroup) {
+        if (!userGroup.isCaptain()) {
+            throw new NoAuthorizedNoticeArticleException();
+        }
+    }
+}
