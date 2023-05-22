720da721d93482f8326111c8d7aac325d217ae09
jihwan2da <wlghks8982@naver.com>
2023-05-22 19:02:46 +0900

    refactor: rename Handler -> QueryHandler

--- src/main/java/com/gloddy/server/comment/domain/handler/CommentQueryHandler.java
+++ src/main/java/com/gloddy/server/comment/domain/handler/CommentQueryHandler.java
@@ -2,6 +2,6 @@ package com.gloddy.server.comment.domain.handler;
 
 import com.gloddy.server.comment.domain.Comment;
 
-public interface CommendHandler {
+public interface CommendQueryHandler {
     Comment findById(Long id);
 }
