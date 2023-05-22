1c4f8b2c1202d27cddff56b6c1944dc23673b8b3
jihwan2da <wlghks8982@naver.com>
2023-05-22 17:43:43 +0900

    refactor: Article 페이징 처리 쿼리 다시 작성(페치 조인)

--- src/main/java/com/gloddy/server/article/infra/repository/ArticleJpaRepository.java
+++ src/main/java/com/gloddy/server/article/infra/repository/ArticleJpaRepository.java
@@ -1,6 +1,7 @@
 package com.gloddy.server.article.infra.repository;
 
 import com.gloddy.server.article.domain.Article;
+import com.gloddy.server.article.infra.repository.custom.ArticleJpaRepositoryCustom;
 import com.gloddy.server.group.domain.Group;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleJpaRepository extends JpaRepository<Article, Long> {
+public interface ArticleJpaRepository extends JpaRepository<Article, Long>, ArticleJpaRepositoryCustom {
     Page<Article> findAllByGroup(Group group, Pageable pageable);
 }
