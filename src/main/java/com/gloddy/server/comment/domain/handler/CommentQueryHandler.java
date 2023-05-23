6a2670a510383c910501837f3cd5bc4906d2346a
jihwan2da <wlghks8982@naver.com>
2023-05-23 19:23:32 +0900

    feat: 댓글 조회 도메인 단 메서드 생성

--- src/main/java/com/gloddy/server/comment/domain/handler/CommentQueryHandler.java
+++ src/main/java/com/gloddy/server/comment/domain/handler/CommentQueryHandler.java
@@ -1,7 +1,11 @@
 package com.gloddy.server.comment.domain.handler;
 
+import com.gloddy.server.article.domain.Article;
 import com.gloddy.server.comment.domain.Comment;
+
+import java.util.List;
 
 public interface CommentQueryHandler {
     Comment findById(Long id);
+    List<Comment> findAllByArticleFetchUser(Article article);
 }
--- src/main/java/com/gloddy/server/comment/domain/handler/impl/CommentQueryHandlerImpl.java
+++ src/main/java/com/gloddy/server/comment/domain/handler/impl/CommentQueryHandlerImpl.java
@@ -1,11 +1,14 @@
 package com.gloddy.server.comment.domain.handler.impl;
 
+import com.gloddy.server.article.domain.Article;
 import com.gloddy.server.comment.domain.Comment;
 import com.gloddy.server.comment.domain.handler.CommentQueryHandler;
 import com.gloddy.server.comment.exception.NotFoundCommentException;
 import com.gloddy.server.comment.infra.repository.CommentJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -17,5 +20,10 @@ public class CommentQueryHandlerImpl implements CommentQueryHandler {
     public Comment findById(Long id) {
         return commentJpaRepository.findById(id)
                 .orElseThrow(NotFoundCommentException::new);
+    }
+
+    @Override
+    public List<Comment> findAllByArticleFetchUser(Article article) {
+        return commentJpaRepository.findAllByArticleFetchUser(article);
     }
 }
