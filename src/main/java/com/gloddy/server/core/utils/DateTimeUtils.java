e4d559cfb75af56595f8907f28608c1b2ad20272
jihwan2da <wlghks8982@naver.com>
2023-05-26 18:43:38 +0900

    feat: Group 조회 용 DateTime 유틸 메소드 추가

--- src/main/java/com/gloddy/server/core/utils/DateTimeUtils.java
+++ src/main/java/com/gloddy/server/core/utils/DateTimeUtils.java
@@ -1,8 +1,11 @@
 package com.gloddy.server.core.utils;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
 import java.util.Optional;
 
 import static java.time.LocalDateTime.*;
@@ -22,5 +25,14 @@ public class DateTimeUtils {
         return Optional.ofNullable(date)
                 .orElse(now())
                 .format(ofPattern("yyyy-MM-dd HH:mm"));
+    }
+
+    public static String dateToStringForGroupPreview(LocalDate date) {
+        LocalDate localDate = Optional.ofNullable(date)
+                .orElse(LocalDate.now());
+
+        DayOfWeek dayOfWeek = localDate.getDayOfWeek();
+        String day = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.US);
+        return localDate + " " + day;
     }
 }
