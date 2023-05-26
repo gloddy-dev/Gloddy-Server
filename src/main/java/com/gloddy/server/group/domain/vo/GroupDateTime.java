f2046300731d2b0e936d65e8a48537376ebb7431
jihwan2da <wlghks8982@naver.com>
2023-05-26 16:56:43 +0900

    feat: GroupDateTime VO 생성

---
+++ src/main/java/com/gloddy/server/group/domain/vo/GroupDateTime.java
@@ -0,0 +1,33 @@
+package com.gloddy.server.group.domain.vo;
+
+import com.gloddy.server.core.utils.DateTimeUtils;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import static com.gloddy.server.core.utils.DateTimeUtils.*;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+public class GroupDateTime {
+
+    @Column(name = "start_date_time")
+    private LocalDateTime startDateTime;
+
+    @Column(name = "end_date_time")
+    private LocalDateTime endDateTime;
+
+    public static GroupDateTime createFrom(LocalDate meetDate, String startTime, String endTime) {
+        LocalDateTime startDateTime = concatDateAndTime(meetDate, stringToLocalTime(startTime));
+        LocalDateTime endDateTime = concatDateAndTime(meetDate, stringToLocalTime(endTime));
+        return new GroupDateTime(startDateTime, endDateTime);
+    }
+}
