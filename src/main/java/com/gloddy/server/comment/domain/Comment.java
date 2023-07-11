4135c585862c04fca0ea9620154687ac8387bec5
jihwan2da <wlghks8982@naver.com>
2023-07-11 21:38:03 +0900

    fix: Comment-User Many-To-One 관계로 변경

--- src/main/java/com/gloddy/server/comment/domain/Comment.java
+++ src/main/java/com/gloddy/server/comment/domain/Comment.java
@@ -24,7 +24,7 @@ public class Comment extends BaseTimeEntity {
     @JoinColumn(name = "article_id")
     private Article article;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
