1af3678274a1051b92cea120f8fa0ffc0370b0b7
jihwan2da <wlghks8982@naver.com>
2023-07-11 21:34:43 +0900

    refactor: jwt 버전 변경에 따른 Migration

--- src/main/java/com/gloddy/server/auth/jwt/JwtTokenBuilder.java
+++ src/main/java/com/gloddy/server/auth/jwt/JwtTokenBuilder.java
@@ -3,9 +3,12 @@ package com.gloddy.server.auth.jwt;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -31,7 +34,12 @@ public class JwtTokenBuilder {
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(new Date(now.getTime()+ vaildTimeMilli))
-                .signWith(SignatureAlgorithm.HS256, key)
+                .signWith(getSigningKey(key), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    private Key getSigningKey(String secretKey) {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
