4ed713871e9599284bd7f5dc701ad86b37a1bd48
jihwan2da <wlghks8982@naver.com>
2023-07-11 20:21:14 +0900

    refactor: swagger springfox -> springdoc

--- src/main/java/com/gloddy/server/config/SwaggerConfig.java
+++ src/main/java/com/gloddy/server/config/SwaggerConfig.java
@@ -1,46 +1,24 @@
 package com.gloddy.server.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.List;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
-    public Docket gloddyApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.gloddy.server"))
-                .build()
-                .securitySchemes(List.of(apiKey()))
-                .securityContexts(List.of(securityContext()));
-    }
-
-    private SecurityContext securityContext() {
-        return SecurityContext.builder().securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
-    }
-
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", "X-AUTH-TOKEN", "header");
-    }
-
-
-    private List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope = new AuthorizationScope("glbal", "access All");
-        AuthorizationScope[] authorizationScopes = {authorizationScope};
-
-        return List.of(new SecurityReference("JWT", authorizationScopes));
+    public OpenAPI gloddyOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("JWT",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("X-AUTH-TOKEN")))
+                .addSecurityItem(new SecurityRequirement().addList("JWT"));
     }
 }
