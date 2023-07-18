57563997735ec963d83c29baae1afd2dbf7149e9
twoosky <lhjae3@naver.com>
2023-07-18 16:13:20 +0900

    feat: 인증코드 확인 비즈니스 로직 구현

--- src/main/java/com/gloddy/server/authSms/application/SmsService.java
+++ src/main/java/com/gloddy/server/authSms/application/SmsService.java
@@ -1,7 +1,9 @@
-package com.gloddy.server.authSms.infra.application;
+package com.gloddy.server.authSms.application;
 
-import com.gloddy.server.authSms.infra.SmsClient;
-import com.gloddy.server.authSms.infra.dto.SmsRequest;
+import com.gloddy.server.authSms.domain.SmsClient;
+import com.gloddy.server.authSms.domain.dto.SmsRequest;
+import com.gloddy.server.authSms.domain.dto.SmsResponse;
+import com.gloddy.server.authSms.utils.VerificationCodeUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,8 +12,16 @@ import org.springframework.stereotype.Service;
 public class SmsService {
 
     private final SmsClient smsClient;
+    private final VerificationCodeUtil verificationCodeUtil;
 
     public void send(SmsRequest.Send request) {
         smsClient.send(request);
+    }
+
+    public SmsResponse.VerifyCode verifyCode(SmsRequest.VerifyCode request) {
+        return new SmsResponse.VerifyCode(verificationCodeUtil.verify(
+                request.getNumber(),
+                request.getCode()
+        ));
     }
 }
