4347fc6a6b3d969ae347457aad1031b7102d9d80
twoosky <lhjae3@naver.com>
2023-07-18 16:16:49 +0900

    feat: errorDecoder 구현체 bean으로 등록

--- src/main/java/com/gloddy/server/config/OpenFeignConfig.java
+++ src/main/java/com/gloddy/server/config/OpenFeignConfig.java
@@ -1,6 +1,8 @@
 package com.gloddy.server.config;
 
+import com.gloddy.server.authSms.infra.ncloud.SmsFeignError;
 import feign.Logger;
+import feign.codec.ErrorDecoder;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +14,10 @@ public class OpenFeignConfig {
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return new SmsFeignError();
     }
 }
