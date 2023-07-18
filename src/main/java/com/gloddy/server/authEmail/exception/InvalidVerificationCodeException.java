87b76473ff3c00eb9373bf9333ffe91e3212dee1
twoosky <lhjae3@naver.com>
2023-07-18 23:05:54 +0900

    fix: 올바르지 않은 인증코드 errorCode명 수정

--- src/main/java/com/gloddy/server/authEmail/exception/InvalidVerificationCodeException.java
+++ src/main/java/com/gloddy/server/authEmail/exception/InvalidVerificationCodeException.java
@@ -6,7 +6,7 @@ import com.gloddy.server.core.error.handler.exception.UserBusinessException;
 public class InvalidVerificationCodeException extends UserBusinessException {
 
     public InvalidVerificationCodeException() {
-        super(ErrorCode.CODE_INVALID);
+        super(ErrorCode.VERIFICATION_CODE_INVALID);
     }
 
 }
--- src/main/java/com/gloddy/server/authSms/infra/ncloud/SmsFeignError.java
+++ src/main/java/com/gloddy/server/authSms/infra/ncloud/SmsFeignError.java
@@ -1,8 +1,7 @@
 package com.gloddy.server.authSms.infra.ncloud;
 
 import com.gloddy.server.core.error.handler.errorCode.ErrorCode;
-import com.gloddy.server.core.error.handler.exception.SmsBadRequestException;
-import com.gloddy.server.core.error.handler.exception.SmsUnauthorizedException;
+import com.gloddy.server.core.error.handler.exception.SmsBusinessException;
 import feign.Response;
 import feign.codec.ErrorDecoder;
 import lombok.extern.slf4j.Slf4j;
@@ -12,8 +11,8 @@ public class SmsFeignError implements ErrorDecoder {
     @Override
     public Exception decode(String methodKey, Response response) {
         return switch (response.status()) {
-            case 400 -> new SmsBadRequestException(ErrorCode.SMS_BAD_REQUEST);
-            case 401 -> new SmsUnauthorizedException(ErrorCode.SMS_UNAUTHORIZED);
+            case 400 -> new SmsBusinessException(ErrorCode.SMS_BAD_REQUEST);
+            case 401 -> new SmsBusinessException(ErrorCode.SMS_UNAUTHORIZED);
             default -> new RuntimeException(response.reason());
         };
     }
