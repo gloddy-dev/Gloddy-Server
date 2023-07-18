7c34522063a1844f5311cdfe434183b769664e52
twoosky <lhjae3@naver.com>
2023-07-18 23:07:25 +0900

    refactor: 핸드폰 번호 검증 로직 추가

--- src/main/java/com/gloddy/server/authSms/application/SmsService.java
+++ src/main/java/com/gloddy/server/authSms/application/SmsService.java
@@ -4,7 +4,10 @@ import com.gloddy.server.authSms.domain.SmsClient;
 import com.gloddy.server.authSms.domain.dto.SmsRequest;
 import com.gloddy.server.authSms.domain.dto.SmsResponse;
 import com.gloddy.server.authSms.utils.VerificationCodeUtil;
+import com.gloddy.server.core.error.handler.errorCode.ErrorCode;
+import com.gloddy.server.core.error.handler.exception.SmsBusinessException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,13 +18,21 @@ public class SmsService {
     private final VerificationCodeUtil verificationCodeUtil;
 
     public void send(SmsRequest.Send request) {
+        validateNumber(request.getNumber());
         smsClient.send(request);
     }
 
     public SmsResponse.VerifyCode verifyCode(SmsRequest.VerifyCode request) {
-        return new SmsResponse.VerifyCode(verificationCodeUtil.verify(
-                request.getNumber(),
-                request.getCode()
-        ));
+        validateNumber(request.getNumber());
+        verificationCodeUtil.verify(request.getNumber(), request.getCode());
+        return new SmsResponse.VerifyCode(true);
+    }
+
+    private void validateNumber(String number) {
+        try {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
+            throw new SmsBusinessException(ErrorCode.PHONE_NUMBER_INVALID);
+        }
     }
 }
