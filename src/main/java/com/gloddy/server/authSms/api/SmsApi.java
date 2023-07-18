dba665ec52986edaacb046be480de1708683ad0f
twoosky <lhjae3@naver.com>
2023-07-18 23:38:54 +0900

    refactor: valid 적용해 핸드폰 번호 검증

--- src/main/java/com/gloddy/server/authSms/api/SmsApi.java
+++ src/main/java/com/gloddy/server/authSms/api/SmsApi.java
@@ -5,8 +5,8 @@ import com.gloddy.server.authSms.domain.dto.SmsResponse;
 import com.gloddy.server.authSms.application.SmsService;
 import com.gloddy.server.authSms.domain.dto.SmsRequest;
 import com.gloddy.server.core.response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,13 +20,13 @@ public class SmsApi {
     private final SmsService smsService;
 
     @PostMapping("/auth/sms")
-    public ResponseEntity<Void> send(@RequestBody SmsRequest.Send request) {
+    public ResponseEntity<Void> send(@RequestBody @Valid SmsRequest.Send request) {
         smsService.send(request);
         return ApiResponse.noContent();
     }
 
     @PostMapping("/auth/sms/verify-code")
-    public ResponseEntity<SmsResponse.VerifyCode> verifyCode(@RequestBody SmsRequest.VerifyCode request) {
+    public ResponseEntity<SmsResponse.VerifyCode> verifyCode(@RequestBody @Valid SmsRequest.VerifyCode request) {
         SmsResponse.VerifyCode response = smsService.verifyCode(request);
         return ApiResponse.ok(response);
     }
--- src/main/java/com/gloddy/server/authSms/application/SmsService.java
+++ src/main/java/com/gloddy/server/authSms/application/SmsService.java
@@ -16,23 +16,14 @@ public class SmsService {
 
     private final SmsClient smsClient;
     private final VerificationCodeUtil verificationCodeUtil;
+    public static int PHONE_NUMBER_LENGTH = 11;
 
     public void send(SmsRequest.Send request) {
-        validateNumber(request.getNumber());
         smsClient.send(request);
     }
 
     public SmsResponse.VerifyCode verifyCode(SmsRequest.VerifyCode request) {
-        validateNumber(request.getNumber());
         verificationCodeUtil.verify(request.getNumber(), request.getCode());
         return new SmsResponse.VerifyCode(true);
-    }
-
-    private void validateNumber(String number) {
-        try {
-            Integer.parseInt(number);
-        } catch (NumberFormatException e) {
-            throw new SmsBusinessException(ErrorCode.PHONE_NUMBER_INVALID);
-        }
     }
 }
