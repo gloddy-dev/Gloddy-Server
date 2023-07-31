1e0250256797e60205ede17ad15ea1fb5f22e6ff
twoosky <lhjae3@naver.com>
2023-08-01 02:56:44 +0900

    refactor: 프로필 정보 임베디드 필드로 분리

--- src/main/java/com/gloddy/server/user/application/UserGetService.java
+++ src/main/java/com/gloddy/server/user/application/UserGetService.java
@@ -1,19 +1,20 @@
 package com.gloddy.server.user.application;
 
 import com.gloddy.server.auth.domain.User;
-import com.gloddy.server.auth.domain.vo.kind.Personality;
 import com.gloddy.server.core.error.handler.errorCode.ErrorCode;
 import com.gloddy.server.core.error.handler.exception.PraiseBusinessException;
-import com.gloddy.server.core.utils.TimeUtil;
 import com.gloddy.server.praise.domain.Praise;
 import com.gloddy.server.mate.infra.repository.MateJpaRepository;
 import com.gloddy.server.praise.infra.repository.PraiseJpaRepository;
+import com.gloddy.server.reliability.domain.Reliability;
+import com.gloddy.server.reliability.domain.handler.ReliabilityQueryHandler;
+import com.gloddy.server.user.domain.dto.UserGetResponse;
 import com.gloddy.server.user.domain.handler.UserQueryHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.gloddy.server.user.domain.dto.UserResponse.*;
+import static com.gloddy.server.user.domain.dto.UserGetResponse.*;
 
 @Service
 @RequiredArgsConstructor
@@ -21,24 +22,21 @@ public class UserService {
     private final UserQueryHandler userQueryHandler;
     private final PraiseJpaRepository praiseJpaRepository;
     private final MateJpaRepository mateJpaRepository;
+    private final ReliabilityQueryHandler reliabilityQueryHandler;
 
     @Transactional(readOnly = true)
-    public MyPage getMyPage(Long userId) {
+    public UserGetResponse getMyPage(Long userId) {
         User user = userQueryHandler.findById(userId);
         Praise praise = praiseJpaRepository.findByUserId(userId)
                 .orElseThrow(() -> new PraiseBusinessException(ErrorCode.PRAISE_NOT_FOUND));
+        Reliability reliability = reliabilityQueryHandler.findByUserId(userId);
         int reviewCount = mateJpaRepository.countByMateId(userId).intValue();
 
-        return new MyPage(
-                user.getImageUrl(),
-                user.getNickname(),
-                user.getGender().toString(),
-                TimeUtil.calculateAge(user.getBirth()),
-                user.getSchool(),
+        return UserGetResponse.of(
+                user,
+                reliability,
                 sumPraiseCount(praise),
-                reviewCount,
-                user.getIntroduce(),
-                Personality.of(user.getPersonalities())
+                reviewCount
         );
     }
 
--- src/main/java/com/gloddy/server/user/domain/dto/UserGetResponse.java
+++ src/main/java/com/gloddy/server/user/domain/dto/UserGetResponse.java
@@ -1,23 +1,38 @@
 package com.gloddy.server.user.domain.dto;
 
+import com.gloddy.server.reliability.domain.vo.ReliabilityLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public class UserResponse {
     @AllArgsConstructor
     @Getter
-    public static class MyPage {
+    public static class Get {
         private String imageUrl;
         private String name;
         private String gender;
         private int age;
         private String school;
-//        private int score;  // 신뢰도
+        private ReliabilityLevel level;
         private int praiseCount;
         private int reviewCount;
         private String introduce;
         private List<String> personalities;
     }
+
+    @AllArgsConstructor
+    @Getter
+    public static class Update {
+        private String imageUrl;
+        private String name;
+        private LocalDate birth;
+        private String gender;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class
 }
