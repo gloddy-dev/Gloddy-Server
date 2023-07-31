dfcef20611b43fe3fbb43fee1aea8196f554a4c6
twoosky <lhjae3@naver.com>
2023-08-01 03:01:20 +0900

    refactor: 유저 조회 서비스 및 dto 분리

--- src/main/java/com/gloddy/server/user/domain/dto/UserGetResponse.java
+++ src/main/java/com/gloddy/server/user/domain/dto/UserGetResponse.java
@@ -1,38 +1,44 @@
 package com.gloddy.server.user.domain.dto;
 
+import com.gloddy.server.auth.domain.User;
+import com.gloddy.server.auth.domain.vo.Profile;
+import com.gloddy.server.auth.domain.vo.kind.Personality;
+import com.gloddy.server.core.utils.TimeUtil;
+import com.gloddy.server.reliability.domain.Reliability;
 import com.gloddy.server.reliability.domain.vo.ReliabilityLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.time.LocalDate;
 import java.util.List;
 
-public class UserResponse {
-    @AllArgsConstructor
-    @Getter
-    public static class Get {
-        private String imageUrl;
-        private String name;
-        private String gender;
-        private int age;
-        private String school;
-        private ReliabilityLevel level;
-        private int praiseCount;
-        private int reviewCount;
-        private String introduce;
-        private List<String> personalities;
-    }
+@AllArgsConstructor
+@Getter
+public class UserGetResponse {
+    private String imageUrl;
+    private String name;
+    private String gender;
+    private int age;
+    private String school;
+    private ReliabilityLevel reliability;
+    private int praiseCount;
+    private int reviewCount;
+    private String introduce;
+    private List<String> personalities;
 
-    @AllArgsConstructor
-    @Getter
-    public static class Update {
-        private String imageUrl;
-        private String name;
-        private LocalDate birth;
-        private String gender;
+    public static UserGetResponse of(User user, Profile profile, Reliability reliability,
+                                     int praiseCount, int reviewCount
+    ) {
+        return new UserGetResponse(
+            profile.getImageUrl(),
+            profile.getNickname(),
+            profile.getGender().toString(),
+            TimeUtil.calculateAge(profile.getBirth()),
+            user.getSchool(),
+            reliability.getLevel(),
+            praiseCount,
+            reviewCount,
+            profile.getIntroduce(),
+            Personality.of(profile.getPersonalities())
+        );
     }
-
-    @AllArgsConstructor
-    @Getter
-    public static class
 }
