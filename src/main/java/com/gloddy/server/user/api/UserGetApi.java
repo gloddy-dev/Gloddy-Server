b69123709c5c9cdbd878aec69ff57a59452174f4
twoosky <lhjae3@naver.com>
2023-08-01 02:59:29 +0900

    feat: 프로필 수정 api 작성

--- src/main/java/com/gloddy/server/user/api/UserGetApi.java
+++ src/main/java/com/gloddy/server/user/api/UserGetApi.java
@@ -4,7 +4,7 @@ import com.gloddy.server.core.response.ApiResponse;
 import com.gloddy.server.mate.application.MateService;
 import com.gloddy.server.praise.application.PraiseService;
 import com.gloddy.server.user.domain.dto.UserGetResponse;
-import com.gloddy.server.user.application.UserService;
+import com.gloddy.server.user.application.UserGetService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,21 +17,17 @@ import static com.gloddy.server.praise.domain.dto.PraiseResponse.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/")
-public class UserApi {
-    private final UserService userService;
+public class UserGetApi {
+    private final UserGetService userService;
     private final PraiseService praiseService;
     private final MateService mateService;
 
     @Operation(summary = "마이페이지 조회")
     @GetMapping("/me/page")
-    public ResponseEntity<UserGetResponse.MyPage> getMyPage(@AuthenticationPrincipal Long userId) {
-        UserGetResponse.MyPage response = userService.getMyPage(userId);
+    public ResponseEntity<UserGetResponse> getMyPage(@AuthenticationPrincipal Long userId) {
+        UserGetResponse response = userService.getMyPage(userId);
         return ApiResponse.ok(response);
     }
-
-    @Operation(summary = "마이페이지 수정")
-    @PatchMapping("/me/info")
-    public ResponseEntity<UserGetResponse.MyPage>
 
     @Operation(summary = "마이페이지 칭찬 조회")
     @GetMapping("/me/praises")
