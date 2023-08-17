77a2f8cc3e8a9c2cc78f04a8b3b42aeb8616677e
twoosky <lhjae3@naver.com>
2023-08-18 02:30:32 +0900

    test: group request dto mock 데이터 수정

--- src/test/java/com/gloddy/server/common/reliability/ReliabilityApiTest.java
+++ src/test/java/com/gloddy/server/common/reliability/ReliabilityApiTest.java
@@ -67,6 +67,7 @@ public abstract class ReliabilityApiTest extends BaseApiTest {
                 "12:00",
                 "15:00",
                 "숭실대학교",
+                "동작구 상도로",
                 "150",
                 "150",
                 10
