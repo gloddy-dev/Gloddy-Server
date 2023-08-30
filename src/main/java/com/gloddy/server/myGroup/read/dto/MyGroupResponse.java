0c01dfea4d24bb240816f06480caa09bb491a06e
jihwan2da <wlghks8982@naver.com>
2023-08-31 03:54:55 +0900

    chore: 나의 모임 조회 Response Swagger Schema 반영

--- src/main/java/com/gloddy/server/myGroup/read/dto/MyGroupResponse.java
+++ src/main/java/com/gloddy/server/myGroup/read/dto/MyGroupResponse.java
@@ -1,5 +1,6 @@
 package com.gloddy.server.myGroup.read.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,7 @@ public class MyGroupResponse {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(name = "MyGroupGetGroupInfoResponse")
     public static class GroupInfo {
         private Long groupId;
         private String imageUrl;
@@ -28,6 +30,7 @@ public class MyGroupResponse {
     @Getter
     @AllArgsConstructor
     @NoArgsConstructor
+    @Schema(name = "MyGroupGetParticipatingResponse")
     public static class Participating {
 
         private List<One> groups;
@@ -35,6 +38,7 @@ public class MyGroupResponse {
         @Getter
         @AllArgsConstructor
         @NoArgsConstructor
+        @Schema(name = "MyGroupGetParticipatingOneResponse")
         public static class One {
             private Boolean isNew;
             private GroupInfo group;
@@ -44,12 +48,14 @@ public class MyGroupResponse {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(name = "MyGroupGetHostingResponse")
     public static class Hosting {
         private List<One> groups;
 
         @Getter
         @NoArgsConstructor
         @AllArgsConstructor
+        @Schema(name = "MyGroupGetHostingOneResponse")
         public static class One {
             private Boolean isExistNewApply;
             private GroupInfo group;
@@ -59,11 +65,13 @@ public class MyGroupResponse {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(name = "MyGroupGetWaitingResponse")
     public static class Waiting {
         private List<One> groups;
         @Getter
         @NoArgsConstructor
         @AllArgsConstructor
+        @Schema(name = "MyGroupGetWaitingOneResponse")
         public static class One {
             private GroupInfo group;
         }
@@ -72,12 +80,14 @@ public class MyGroupResponse {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(name = "MyGroupGetRejectedResponse")
     public static class Rejected {
         private List<One> groups;
 
         @Getter
         @NoArgsConstructor
         @AllArgsConstructor
+        @Schema(name = "MyGroupGetRejectedOneResponse")
         public static class One {
             private Long applyId;
             private GroupInfo group;
@@ -87,12 +97,14 @@ public class MyGroupResponse {
     @Getter
     @NoArgsConstructor
     @AllArgsConstructor
+    @Schema(name = "MyGroupGetNotEstimatedResponse")
     public static class NotEstimated {
         private List<One> groups;
 
         @Getter
         @NoArgsConstructor
         @AllArgsConstructor
+        @Schema(name = "MyGroupGetNotEstimatedOneResponse")
         public static class One {
             private Boolean isCaptain;
             private GroupInfo group;
