b464f1456afe85310643a47670fb5c9f6486eeca
jihwan2da <wlghks8982@naver.com>
2023-09-03 15:23:59 +0900

    test: 닉네임 중복 검사 ServiceTest 작성

---
+++ src/test/java/com/gloddy/server/service/users/ExistNicknameTest.java
@@ -0,0 +1,40 @@
+package com.gloddy.server.service.users;
+
+import com.gloddy.server.auth.domain.User;
+import com.gloddy.server.common.users.UserServiceTest;
+import com.gloddy.server.user.api.dto.UserResponse;
+import com.gloddy.server.user.application.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExistNicknameTest extends UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void existNickname_returns_true() {
+        //given
+        Long userId = createUser();
+        User user = userJpaRepository.findById(userId).orElseThrow();
+        String nickName = user.getNickName();
+
+        //when
+        UserResponse.ExistNickname existNickname = userService.existNickname(nickName);
+
+        //then
+        assertTrue(existNickname.getIsExistNickname());
+    }
+
+    @Test
+    void existNickname_returns_false() {
+        //when
+        UserResponse.ExistNickname existNickname = userService.existNickname("test");
+
+        //then
+        assertFalse(existNickname.getIsExistNickname());
+    }
+}
