b7d711f5b042c1ce4649ea10de24098d49239370
jihwan2da <wlghks8982@naver.com>
2023-09-05 18:59:05 +0900

    feat: 참여한 그룹 Count를 위한 쿼리 작성

--- src/main/java/com/gloddy/server/group_member/infra/repository/custom/GroupMemberJpaRepositoryCustom.java
+++ src/main/java/com/gloddy/server/group_member/infra/repository/custom/GroupMemberJpaRepositoryCustom.java
@@ -6,6 +6,7 @@ import com.gloddy.server.group_member.domain.GroupMember;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface GroupMemberJpaRepositoryCustom {
@@ -19,4 +20,6 @@ public interface GroupMemberJpaRepositoryCustom {
     List<GroupMember> findAllByGroupIdFetchUserAndGroup(Long groupId);
 
     List<GroupMember> findByUserIdFetchGroupAndUser(Long userId);
+
+    Long countByUserIdAndIsAbsenceAndEndDateTimeBeforeJoinGroup(Long userId, boolean isAbsence, LocalDateTime time);
 }
