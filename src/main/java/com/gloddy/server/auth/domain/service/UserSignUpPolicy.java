65ccdab8660fc4b369c4fcaccd3cff74532a8e51
twoosky <lhjae3@naver.com>
2023-09-06 19:58:01 +0900

    feat: signout validator 작성

--- src/main/java/com/gloddy/server/auth/domain/service/UserSignUpPolicy.java
+++ src/main/java/com/gloddy/server/auth/domain/service/UserSignUpPolicy.java
@@ -1,27 +1,21 @@
 package com.gloddy.server.auth.domain.service;
 
-import com.gloddy.server.auth.domain.User;
 import com.gloddy.server.auth.domain.vo.Phone;
 import com.gloddy.server.auth.exception.AlreadyUserSignUpException;
-import com.gloddy.server.user.infra.repository.UserJpaRepository;
+import com.gloddy.server.user.domain.handler.UserQueryHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class UserSignUpPolicy {
 
-    private final UserJpaRepository userJpaRepository;
+    private final UserQueryHandler userQueryHandler;
 
     public void validate(String phoneNumber) {
         Phone phone = new Phone(phoneNumber);
 
-        Optional<User> user = userJpaRepository.findByPhone(phone);
-
-        if (user.isPresent()) {
-            throw new AlreadyUserSignUpException();
-        }
+        userQueryHandler.findByPhone(phone)
+                .ifPresent(user -> {throw new AlreadyUserSignUpException();});
     }
 }
