867a03f3dfbfe4b80695f2bae83453f12926ee81
jihwan2da <wlghks8982@naver.com>
2023-09-12 03:27:00 +0900

    fix: 리뷰 삭제안되는 이슈 해결(트랜잭션)

--- src/main/java/com/gloddy/server/mate/infra/repository/MateJpaRepository.java
+++ src/main/java/com/gloddy/server/mate/infra/repository/MateJpaRepository.java
@@ -2,11 +2,13 @@ package com.gloddy.server.mate.infra.repository;
 
 import com.gloddy.server.mate.domain.Mate;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface MateJpaRepository extends JpaRepository<Mate, Long> {
     Long countByUserId(Long userId);
     List<Mate> findAllByUserId(Long userId);
+    @Transactional
     void deleteByMateIdAndUserId(Long mateId, Long userId);
 }
