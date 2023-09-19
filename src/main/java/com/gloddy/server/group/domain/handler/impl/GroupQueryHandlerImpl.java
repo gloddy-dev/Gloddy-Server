c7d4793e67ffbf3d23bab73262c6a84070d2929f
twoosky <lhjae3@naver.com>
2023-09-19 11:14:04 +0900

    fix: group 전체 조회 쿼리 수정 (종료된 모임 제외)

--- src/main/java/com/gloddy/server/group/domain/handler/impl/GroupQueryHandlerImpl.java
+++ src/main/java/com/gloddy/server/group/domain/handler/impl/GroupQueryHandlerImpl.java
@@ -31,7 +31,7 @@ public class GroupQueryHandlerImpl implements GroupQueryHandler {
 
     @Override
     public Page<Group> findGroupPreviewPage(Pageable pageable) {
-        return groupJpaRepository.findAllByOrderByIdDesc(pageable);
+        return groupJpaRepository.findAllByEndDateTimeAfterOrderByCreatedAtDesc(LocalDateTime.now(), pageable);
     }
 
     @Override
