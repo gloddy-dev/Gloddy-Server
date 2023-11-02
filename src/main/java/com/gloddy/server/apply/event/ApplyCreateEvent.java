caa8215498a5a0b9d6a6e391be3ebad686dbc43f
jihwan2da <wlghks8982@naver.com>
2023-11-02 23:52:02 +0900

    feat: 기존 로직 Apply Event 재정의

---
+++ src/main/java/com/gloddy/server/apply/event/ApplyCreateEvent.java
@@ -0,0 +1,16 @@
+package com.gloddy.server.apply.event;
+
+import com.gloddy.server.core.event.Event;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+public class ApplyCreateEvent implements Event {
+    private Long userId;
+    private Long groupId;
+    private Long applyId;
+}
---
+++ src/main/java/com/gloddy/server/apply/event/ApplyStatusUpdateEvent.java
@@ -0,0 +1,18 @@
+package com.gloddy.server.apply.event;
+
+import com.gloddy.server.apply.domain.vo.Status;
+import com.gloddy.server.core.event.Event;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ApplyStatusUpdateEvent implements Event {
+    private Long userId;
+    private Long groupId;
+    private Long applyId;
+    private Status status;
+}
