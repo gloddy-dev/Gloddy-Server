5d0ffe51b3e7a5c71a8803d59f4f74fb59e69068
jihwan2da <wlghks8982@naver.com>
2023-11-19 23:49:22 +0900

    refactor: Praise, Reliability Repository 삭제

--- src/main/java/com/gloddy/server/reliability/domain/handler/ReliabilityQueryHandler.java
+++
@@ -1,8 +0,0 @@
-package com.gloddy.server.reliability.domain.handler;
-
-import com.gloddy.server.reliability.domain.Reliability;
-
-public interface ReliabilityQueryHandler {
-    Reliability findByUserId(Long userId);
-    Reliability save(Reliability reliability);
-}
