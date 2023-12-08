8e50837dd96ccb9dd609588c8b896d7eb13a2d51
twoosky <lhjae3@naver.com>
2023-12-08 18:21:59 +0900

    refactor: apply 이벤트 zeroPayload로 리팩토링

--- src/main/java/com/gloddy/server/messaging/adapter/apply/mapper/ApplyEventMapper.java
+++ src/main/java/com/gloddy/server/messaging/adapter/apply/mapper/ApplyEventMapper.java
@@ -5,34 +5,31 @@ import com.gloddy.server.apply.event.ApplyCreateEvent;
 import com.gloddy.server.apply.event.ApplyStatusUpdateEvent;
 import com.gloddy.server.messaging.adapter.apply.event.ApplyAdapterEvent;
 import com.gloddy.server.messaging.adapter.apply.event.ApplyEventType;
+import java.time.LocalDateTime;
 
 public class ApplyEventMapper {
 
     public static ApplyAdapterEvent mapToApplyAdapterEventFrom(ApplyCreateEvent applyCreateEvent) {
         return new ApplyAdapterEvent(
-                applyCreateEvent.getCaptainId(),
-                applyCreateEvent.getGroupId(),
-                applyCreateEvent.getApplyUserId(),
-                ApplyEventType.APPLY_CREATE
+                applyCreateEvent.getApplyId(),
+                ApplyEventType.APPLY_CREATE,
+                LocalDateTime.now()
         );
     }
 
     public static ApplyAdapterEvent mapToApplyAdapterEventFrom(ApplyStatusUpdateEvent applyStatusUpdateEvent) {
         return new ApplyAdapterEvent(
-                applyStatusUpdateEvent.getCaptainId(),
-                applyStatusUpdateEvent.getGroupId(),
-                applyStatusUpdateEvent.getApplyUserId(),
-                getApplyEventType(applyStatusUpdateEvent.getStatus())
+                applyStatusUpdateEvent.getApplyId(),
+                getApplyEventType(applyStatusUpdateEvent.getStatus()),
+                LocalDateTime.now()
         );
     }
 
     private static ApplyEventType getApplyEventType(Status status) {
-        if (status.isApprove()) {
-            return ApplyEventType.APPLY_APPROVE;
-        } else if (status.isRefuse()) {
-            return ApplyEventType.APPLY_REFUSE;
-        } else {
-            throw new RuntimeException("invalid apply status");
-        }
+        return switch (status) {
+            case APPROVE -> ApplyEventType.APPLY_APPROVE;
+            case REFUSE -> ApplyEventType.APPLY_REFUSE;
+            default -> throw new RuntimeException("invalid apply status");
+        };
     }
 }
