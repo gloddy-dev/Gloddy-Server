a24672f2257d3e351a8a87903bbae2883d2ee0f7
twoosky <lhjae3@naver.com>
2023-12-12 23:55:12 +0900

    feat: objectMapper 빈으로 등록

---
+++ src/main/java/com/gloddy/server/config/ObjectMapperConfig.java
@@ -0,0 +1,15 @@
+package com.gloddy.server.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().registerModule(new JavaTimeModule());
+    }
+}
