6fc1c5b37dc7ed62e1c3d59f465ebb48d77a683f
twoosky <lhjae3@naver.com>
2023-12-12 23:47:07 +0900

    fix: 이벤트 발행 여부 업데이트 메소드 트랜잭션 제거

--- src/main/java/com/gloddy/server/outbox/domain/handler/impl/OutboxEventCommandHandlerImpl.java
+++ src/main/java/com/gloddy/server/outbox/domain/handler/impl/OutboxEventCommandHandlerImpl.java
@@ -21,7 +21,6 @@ public class OutboxEventCommandHandlerImpl implements OutboxEventCommandHandler 
     }
 
     @Override
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void updatePublished(Long eventId) {
         eventJpaRepository.updatePublished(eventId, LocalDateTime.now());
     }
