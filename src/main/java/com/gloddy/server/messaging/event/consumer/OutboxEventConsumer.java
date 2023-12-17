2753004469c9b77ebc18d2eb93c0bf255a414411
twoosky <lhjae3@naver.com>
2023-12-18 03:07:25 +0900

    feat: 메시징 시스템으로 이벤트 발행 비동기 처리

--- src/main/java/com/gloddy/server/messaging/event/consumer/OutboxEventConsumer.java
+++ src/main/java/com/gloddy/server/messaging/event/consumer/OutboxEventConsumer.java
@@ -1,12 +1,14 @@
 package com.gloddy.server.messaging.event.consumer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static com.gloddy.server.config.AsyncConfig.EVENT_HANDLER_TASK_EXECUTOR;
+
 import com.gloddy.server.messaging.MessagePublisher;
 import com.gloddy.server.outbox.domain.Event;
 import com.gloddy.server.outbox.domain.dto.OutboxEventPayload;
 import com.gloddy.server.outbox.domain.handler.OutboxEventCommandHandler;
 import com.gloddy.server.outbox.domain.handler.OutboxEventQueryHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -19,6 +21,7 @@ public class OutboxEventConsumer {
     private final OutboxEventQueryHandler outboxEventQueryHandler;
     private final OutboxEventCommandHandler outboxEventCommandHandler;
 
+    @Async(EVENT_HANDLER_TASK_EXECUTOR)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handle(OutboxEventPayload payload) {
         Event outboxEvent = outboxEventQueryHandler.findById(payload.getId());
