3c77168fcfc91f4cb5c4c34c9d049be6ddf4a3d4
jihwan2da <wlghks8982@naver.com>
2024-02-02 15:07:51 +0900

    feat: 유저 프로필, 신뢰도 업데이트 도메인 이벤트 정의 및 발행

--- src/main/java/com/gloddy/server/user/api/UserUpdateApi.java
+++ src/main/java/com/gloddy/server/user/api/UserUpdateApi.java
@@ -1,7 +1,7 @@
 package com.gloddy.server.user.api;
 
 import com.gloddy.server.core.response.ApiResponse;
-import com.gloddy.server.user.application.UserUpdateService;
+import com.gloddy.server.user.application.UserCommandService;
 import com.gloddy.server.user.domain.dto.UserUpdateRequest;
 import com.gloddy.server.user.domain.dto.UserUpdateResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,7 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class UserUpdateApi {
-    private final UserUpdateService userUpdateService;
+
+    private final UserCommandService userCommandService;
 
     @Operation(summary = "프로필 수정")
     @PatchMapping("/me/info")
@@ -26,7 +27,7 @@ public class UserUpdateApi {
             @AuthenticationPrincipal Long userId,
             @RequestBody @Valid UserUpdateRequest.Info request
     ) {
-        UserUpdateResponse response = userUpdateService.update(userId, request);
+        UserUpdateResponse response = userCommandService.updateProfile(userId, request);
         return ApiResponse.ok(response);
     }
 }
