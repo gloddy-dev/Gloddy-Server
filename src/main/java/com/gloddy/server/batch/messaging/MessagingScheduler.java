3955c0f5685e66995b93a137745fce9b5614b1ab
jihwan2da <wlghks8982@naver.com>
2024-02-02 17:18:02 +0900

    feat: 발행 실패한 유저 이벤트 재발행 스케줄러 구현

--- src/main/java/com/gloddy/server/batch/messaging/MessagingScheduler.java
+++ src/main/java/com/gloddy/server/batch/messaging/MessagingScheduler.java
@@ -3,6 +3,7 @@ package com.gloddy.server.batch.messaging;
 import com.gloddy.server.batch.messaging.producer.MessagingBatchEventProducer;
 import com.gloddy.server.core.error.handler.exception.BatchBusinessException;
 import com.gloddy.server.outbox.domain.handler.GroupOutboxEventQueryHandler;
+import com.gloddy.server.outbox.domain.handler.UserOutboxEventQueryHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -18,21 +19,29 @@ public class MessagingScheduler {
 
     private final MessagingBatchEventProducer messagingBatchEventProducer;
     private final GroupOutboxEventQueryHandler groupOutboxEventQueryHandler;
+    private final UserOutboxEventQueryHandler userOutboxEventQueryHandler;
 
     @Transactional
     @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
-    public void rePublishEvent() {
+    public void rePublishGroupEvent() {
         executeJob(() -> groupOutboxEventQueryHandler.findAllByNotPublished()
-                .forEach(messagingBatchEventProducer::produceEvent));
+                .forEach(messagingBatchEventProducer::produceEvent), "group");
     }
 
-    private void executeJob(Runnable runnable) {
-        log.info("Messaging rePublishEvent publishGroupEndEvent 스케줄러 시작");
+    @Transactional
+    @Scheduled(cron = "0 * * * * *", zone = "Asia/Seoul")
+    public void rePublishUserEvent() {
+        executeJob(() -> userOutboxEventQueryHandler.findAllByNotPublished()
+                .forEach(messagingBatchEventProducer::produceEvent), "user");
+    }
+
+    private void executeJob(Runnable runnable, String domain) {
+        log.info("Messaging " + domain + " rePublishEvent 스케줄러 시작");
         try {
             runnable.run();
         } catch (Exception e) {
             throw new BatchBusinessException(e);
         }
-        log.info("Messaging rePublishEvent publishGroupEndEvent 스케줄러 완료");
+        log.info("Messaging " + domain + " rePublishEvent 스케줄러 완료");
     }
 }
